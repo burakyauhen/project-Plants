d2e843ea28f8e701325f617f60036faaac75e3c4
Burak Yauhen <burakyauhen@gmail.com>
2023-07-07 10:43:41 +0300

    feat: add project build

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import './main.scss';
+import './fonts/fonts.scss';
+import './template.html';
+
+
+// function component(text: string) {
+//     const element = document.createElement('h1');
+//     element.textContent = text;
+//     return element;
+//   }
+  
+//   document.body.prepend(component('Проект собран на Webpack'));
+
+
+// window.addEventListener("load", () => {
+//     const header = document.createElement("h1");
+//     header.innerText = "Webpack❤️TS"
+  
+//     const body = document.querySelector("body");
+//     body!.appendChild(header);
+//   })
---
+++ webpack.config.js
@@ -0,0 +1,110 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+
+
+
+
+module.exports = {
+
+  entry: path.join(__dirname, 'src', 'index.ts'),
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: path.join('images', '[name].[contenthash][ext]'),
+        },
+      },
+
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+        generator: {
+          filename: path.join('icons', '[name].[contenthash][ext]'),
+        },
+      },
+
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: path.join('[name].[contenthash][ext]'),
+        },
+      },
+
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.[contenthash].js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'template.html'),
+      filename: 'index.html',
+    }),
+
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: ['dist'],
+        },
+      },
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    port: 9000,
+  },
+
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              ['svgo', { name: 'preset-default' }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+};
